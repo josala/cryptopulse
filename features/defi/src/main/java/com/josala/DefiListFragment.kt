37572bdb04aae868a226f96e1f565e8edf296b73
Oriol Sala <josala@outlook.com>
2021-02-16 18:48:12 +0100

    Populate nav host with feature fragments

--- features/defi/src/main/java/com/josala/DefiListFragment.kt
+++ features/defi/src/main/java/com/josala/DefiListFragment.kt
@@ -1,5 +1,21 @@
-package com.josala.defi
+package com.josala
 
-class DefiListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.josala.features.defi.R
+
+class DefiListFragment: Fragment() {
+
     //TODO
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_defi, container, false)
+    }
 }
